b88a26908b988436f0580cd531ff4662e4795e5f
Brad Sawicki <git@bradsawicki.com>
2017-06-16 16:37:11 +0200

    Scroll top btn/js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,25 @@
 $(function(){
 
+  var scrollPageTop = function() {
+    var scrollBtn = $('#scroll-top');
+
+    $(window).scroll(function() {
+      var scrollPosition = $(this).scrollTop();
+
+      if(scrollPosition > 500) {
+        scrollBtn.addClass('js-is-visible');
+      } else if(scrollPosition < 500) {
+        scrollBtn.removeClass('js-is-visible');
+      }
+    });
+
+    scrollBtn.bind('click', function(e) {
+      e.preventDefault();
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      return false;
+    });
+  }
+
   var mobileNav = function() {
     var body = $('body');
     var mobileNavTrigger = $('.nav-toggle');
@@ -23,6 +43,7 @@ $(function(){
     });
   }
 
+  scrollPageTop();
   mobileNav();
   productFilterToggle();
 
